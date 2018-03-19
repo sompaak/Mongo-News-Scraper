f35bc46ca9d723151e570884b66aa4ebebfa11d8
Akhil <aksompalli@gmail.com>
2018-03-19 14:47:31 -0700

    added heroku config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -29,7 +29,8 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/week10PopulaterScraper", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/techcrunch";
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
